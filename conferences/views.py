e0b9b509e30d2bae2d08bf081715a6c926c7ed56
roy <rcdosado@gmail.com>
2017-03-24 15:00:00 +0800

    Add Basic Search functionality for finding conferences

--- conferences/views.py
+++ conferences/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Conference
+from django.db.models import Q
 
+from .models import Conference
 import datetime
 
 class ConferenceList(ListView):
 	model = Conference
 	template_name = 'conferences/index.html'
 	context_object_name = 'conferences'
+	paginate_by = 4 
 
 	# you can filter this to customise the data being outputted
 	
 	def get_queryset(self):
-		# import pdb; pdb.set_trace()
 		# return Conference.objects.filter(start_date__gte=datetime.datetime.now()).order_by('start_date')
-		return Conference.objects.all()
+		query = self.request.GET.get("q")	
+		if query:
+			return Conference.objects.filter(
+				Q(title__icontains=query) |
+				Q(venue__icontains=query) |
+				Q(contact_details__icontains=query) |
+				Q(description__icontains=query) 
+				).order_by('-start_date')
+		return Conference.objects.all().order_by('-start_date')
+		
 
 	# uncomment below, if you want to modify, template data
 
@@ -28,4 +38,3 @@ class ConferenceList(ListView):
 class ConferenceDetail(DetailView):
 	model = Conference 
 	template_name = 'conferences/conference.html'
-
