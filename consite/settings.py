e20e78927d501abddf9a44e1108a7ee01af1d190
roy cyril dosado <rc.dosado@psu.palawan.edu.ph>
2022-09-16 06:53:02 +0000

    fix static files issues

--- consite/settings.py
+++ consite/settings.py
@@ -148,14 +148,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-STATIC_URL = "/static/"
+STATIC_URL = "/assets/"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
 # import pdb; pdb.set_trace()
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "assets"),
 ]
 
