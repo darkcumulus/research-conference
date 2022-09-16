69c0f9c858990224c5e3b17b129e9df5b8e4a6ba
rcdosado <rcdosado@gmail.com>
2022-09-16 15:38:16 +0800

    update django admin interface

--- conferences/apps.py
+++ conferences/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
-
+from suit.apps import DjangoSuitConfig
 
 class ConferencesConfig(AppConfig):
     name = "conferences"
+
+class SuitConfig(DjangoSuitConfig):
+    layout = "horizontal"
